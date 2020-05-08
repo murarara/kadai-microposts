45b61cce6b84903ad7aceaa9c33ec635ddcd4962
murakami <murakami@arara.com>
2020-05-08 02:59:47 +0000

    column processing

--- database/migrations/2020_05_07_200531_create_microposts2_table.php
+++ database/migrations/2020_05_07_200531_create_microposts2_table.php
@@ -15,15 +15,15 @@ class CreateMicroposts2Table extends Migration
     {
         Schema::create('microposts2', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('content');
             $table->timestamps();
+
+            // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('microposts2');
